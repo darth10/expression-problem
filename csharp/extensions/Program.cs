23595fcf048c9e6956752eb398ac129a284494ca
Akhil Wali <akhil.wali.10@gmail.com>
2020-03-03 00:50:59 +1300

    Add more examples

--- csharp/extensions/Program.cs
+++ csharp/extensions/Program.cs
@@ -5,19 +5,26 @@ namespace Extensions
 {
     class Program
     {
+        // TODO move to tests
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             var c1 = new Const(2);
-            // var a1 = new Add(c1, new Const(3));
-            var a1 = new Mult(c1, new Const(3));
-            var a2 = new Mult(new Const(5), a1);
-            var a3 = new Add(new Const(5), a1);
+            var a1 = new Add(c1, new Const(3));
+            var a2 = new Add(new Const(5), a1);
+
+            var m1 = new Mult(new Const(5), a1);
+            var m2 = new Mult(c1, new Const(3));
+
+            var a3 = new Add(new Const(5), m1);
 
             Console.WriteLine(c1.Eval());
+            Console.WriteLine(a1.Eval());
             Console.WriteLine(a2.Eval());
-            Console.WriteLine(a3.Eval());
+            Console.WriteLine(m1.Eval());
+            Console.WriteLine(m2.Eval());
+            Console.WriteLine(a3.Eval()); // fails
         }
     }
 }
