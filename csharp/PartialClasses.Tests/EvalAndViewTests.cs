63dafd6c401e2d4d46c71ce8a5854a68720cfad7
Akhil Wali <akhil.wali.10@gmail.com>
2020-03-05 21:40:46 +1300

    Add PartialClasses.Tests project

---
+++ csharp/PartialClasses.Tests/EvalAndViewTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Extensions.Tests
+{
+    public class EvalAndViewTests
+    {
+        [Fact]
+        public void EvalTest1()
+        {
+
+        }
+    }
+}
