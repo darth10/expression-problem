6cb9ddbe732aa180c244b0d6b2e5cfa18257e4de
Akhil Wali <akhil.wali.10@gmail.com>
2020-04-08 00:06:02 +1200

    Add examples in Functors implementation

--- haskell/src/Functors.hs
+++ haskell/src/Functors.hs
@@ -3,7 +3,7 @@
 
 module Functors (const, (>+<), (>*<), eval, view) where
 
-import Prelude (Double, Functor, String, fmap, otherwise, show, (+), (*), (<))
+import Prelude (Double, Functor, String, fmap, otherwise, show, (+), (*), (<), ($))
 import Text.Printf (printf)
 
 data Const e = Const Double deriving Functor
@@ -13,6 +13,8 @@ data ExprF f = In (f (ExprF f))
 
 foldExpr :: Functor f => (f a -> a) -> ExprF f -> a
 foldExpr f (In t) = f (fmap (foldExpr f) t)
+
+infixr 7 :+:
 
 data (f :+: g) e = Inl (f e) | Inr (g e) deriving Functor
 
@@ -40,6 +42,7 @@ eval expr = foldExpr evalF expr
 -- const x = In (Inl (Const x))
 
 -- infixl 6 >+<
+
 -- (>+<) :: Expr -> Expr -> Expr
 -- x >+< y = In (Inr (Add x y))
 
@@ -73,39 +76,55 @@ instance View Mult where
 
 -- helper functions
 
-infixr 7 :+:
 type Expr = ExprF (Const :+: Add :+: Mult)
 
 const :: Double -> Expr
 const x = In (Inl (Const x))
 
 infixl 6 >+<
+
 (>+<) :: Expr -> Expr -> Expr
 x >+< y = In (Inr (Inl (Add x y)))
 
 infixl 7 >*<
+
 (>*<) :: Expr -> Expr -> Expr
 x >*< y = In (Inr (Inr (Mult x y)))
 
-x1 = const 7
-x2 = const 3
-x3 = const 2
-
--- > let const2 x = In (Inl (Const x))
--- > :t const2
--- const2 :: Double -> ExprF (Const :+: g)
--- > let x1 = const2 7
+-- > let x1 = In (Inl (Const 7))
+-- > :t x1
+-- x1 :: ExprF (Const :+: g)
 -- > eval x1
 -- <interactive>:...: error:
 --     * Ambiguous type variable 'g0' arising from a use of 'eval'
 --       prevents the constraint '(Eval g0)' from being solved.
 --       Probable fix: use a type annotation to specify what 'g0' should be.
+--       These potential instances exist:
+--         instance [safe] (Eval f, Eval g) => Eval (f :+: g)
+--           -- Defined at ...
+--         instance [safe] Eval Add
+--           -- Defined at ...
+--         instance [safe] Eval Const
+--           -- Defined at ...
+--         ...plus one other
 
 getAlgebra x
   | x < 10    = const 2.0
-  | otherwise = const 4 >*< const 5 >+< const 6
+  | otherwise = const 4 >+< const 5
 
 -- :t getAlgebra
 -- getAlgebra
---   :: (Ord a, Num a) =>
---      a -> ExprF (Const :+: (Add :+: Mult))
+--   :: (Ord a, Num a) => a -> Expr
+
+x1 = const 7
+x2 = const 3
+x3 = const 2
+
+-- > eval $ x1 >+< x2
+-- 10.0
+-- > eval $ x1 >*< x3 >+< x2 >*< x3
+-- 20.0
+-- > view $ x1 >+< x2
+-- "(7.0 + 3.0)"
+-- > view $ x1 >*< x3 >+< x2 >*< x3
+-- "((7.0 * 2.0) + (3.0 * 2.0))"
